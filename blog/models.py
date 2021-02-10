b7339da25e707771e38e6b3d1f5dd8c8f14f202d
Marco <rootslowed@gmail.com>
2021-02-10 19:30:20 -0300

    first model - post

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	title: models.CharField(max_length=255)
+	description: models.CharField(max_length=255)
+	content: models.TextField()
+	author: models.ForeignKey(User, on_delete=models.PROTECT)
+	created_at: models.DateField(auto_now_add=True)
