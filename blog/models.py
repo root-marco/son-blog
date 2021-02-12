1691ead7e61721e7b562296d1338c331af124a79
Marco <rootslowed@gmail.com>
2021-02-12 16:43:57 -0300

    reset

--- blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Post(models.Model):
-	title: models.CharField(max_length=255)
-	description: models.CharField(max_length=255)
-	content: models.TextField()
-	author: models.ForeignKey(User, on_delete=models.PROTECT)
-	created_at: models.DateField(auto_now_add=True)
