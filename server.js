1e8cbe0c12619f21b639f90c4e38e6af05a5b662
Marco <rootslowed@gmail.com>
2021-02-12 17:03:30 -0300

    express handlebars initial set-up done

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+import exphbs from 'express-handlebars';
 import express from 'express';
 const app = express();
 
-app.listen(3000)
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.listen(3000);
