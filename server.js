46889cb8f20e52422bb21f4ce874065def8bef25
Marco <rootslowed@gmail.com>
2021-02-12 17:06:14 -0300

    articlesRouter imported

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import exphbs from 'express-handlebars';
 import express from 'express';
+import articleRouter from './routes/articles.js';
+
 const app = express();
 
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-	res.render('index');
-});
 
 app.listen(3000);
