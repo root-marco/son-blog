18fed50da7ef0849e138edc3fe708767893a404f
marco <rootslowed@gmail.com>
2021-02-17 15:06:56 -0300

    articles logic created file

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,13 +1,5 @@
 export const root = (req, res) => {
 
-	const articles = [{
-		title: 'test article',
-		createdAt: Date.now(),
-		description: 'askldnaslkdnsk'
-	}];
-
-	res.render('index', {
-		articles: articles,
-	});
+	
 
 };
