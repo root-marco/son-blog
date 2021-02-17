a6ab9dbce26469f20917c63a0877a699090d3e39
marco <rootslowed@gmail.com>
2021-02-17 15:07:10 -0300

    root route to show articles

--- controllers/root.js
+++ controllers/root.js
@@ -1,5 +1,13 @@
 export const root = (req, res) => {
 
-	res.send('ROOT');
+	const articles = [{
+		title: 'test article',
+		createdAt: new Date().toLocaleDateString('pt-BR'),
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, ut amet blanditiis sequi explicabo, temporibus porro omnis voluptas nesciunt tempore aperiam. Voluptatem, repudiandae assumenda quo perferendis rem voluptas ipsum mollitia.',
+	}];
+
+	res.render('index', {
+		articles: articles,
+	});
 
 };
