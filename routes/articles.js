81a012881c950d152a4bdf37655827e8d774244d
marco <rootslowed@gmail.com>
2021-02-17 15:07:27 -0300

    router get to create a new article

--- routes/articles.js
+++ routes/articles.js
@@ -3,6 +3,6 @@ import * as articles from '../controllers/articles.js';
 
 const router = express.Router();
 
-router.get('/', articles.root);
+router.get('/new', articles.root);
 
 export default router;
