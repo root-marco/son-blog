4ad6e09240208985e55c5278ea831924f8fdcbd7
marco <rootslowed@gmail.com>
2021-02-17 15:42:20 -0300

    callbacks names changed

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ import * as articles from '../controllers/articles.js';
 
 const router = express.Router();
 
-router.get('/new', articles.newArticle);
-router.post('/new', articles.newArticle)
+router.get('/new', articles.getNewArticle);
+router.post('/new', articles.postNewArticle);
 
 export default router;
