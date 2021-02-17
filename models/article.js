bf6fa086039b4b784d95c02e02279f51d33b7cee
marco <rootslowed@gmail.com>
2021-02-17 16:16:24 -0300

    article model created with title, description, markdown and createdAt

---
+++ models/article.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const articleSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	markdown: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+export default mongoose.model('article', articleSchema);
