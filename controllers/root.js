34b3a2fabc013acaddc14649d82cb05c903716b0
marco <rootslowed@gmail.com>
2021-02-17 15:11:17 -0300

    rendering articles/index

--- controllers/root.js
+++ controllers/root.js
@@ -6,7 +6,7 @@ export const root = (req, res) => {
 		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, ut amet blanditiis sequi explicabo, temporibus porro omnis voluptas nesciunt tempore aperiam. Voluptatem, repudiandae assumenda quo perferendis rem voluptas ipsum mollitia.',
 	}];
 
-	res.render('index', {
+	res.render('articles/index', {
 		articles: articles,
 	});
 
