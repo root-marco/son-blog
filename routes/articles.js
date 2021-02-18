d8304cc37d5255866d2f1b63790e50a9697fd2a7
marco <rootslowed@gmail.com>
2021-02-18 15:47:13 -0300

    router to get an article by ID

--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,7 @@ import * as articles from '../controllers/articles.js';
 const router = express.Router();
 
 router.get('/new', articles.getNewArticle);
-router.post('/new', articles.postNewArticle);
+router.post('/', articles.postNewArticle);
+router.get(':id', articles.articleId);
 
 export default router;
