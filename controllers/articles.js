2e9647b669bd95b75ce80a293d84f7b8167ce8ff
marco <rootslowed@gmail.com>
2021-02-18 15:20:59 -0300

    logic to create a new post implemented

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,11 +1,32 @@
+import Article from '../models/article.js';
+
 export const getNewArticle = (req, res) => {
 
 	res.render('articles/new');
 
 };
 
-export const postNewArticle = (req, res) => {
+export const postNewArticle = async (req, res) => {
 
-	
+	const article = new Article({
+		title: req.body.title,
+		description: req.body.description,
+		markdown: req.body.markdown,
+	});
+
+	try {
+		article = await article.save();
+		res.redirect(`/articles/${article.id}`);
+	} catch (error) {
+		res.render('articles/new', {
+			article: article,
+		});
+	};
+
+};
+
+export const articleId = (req, res) => {
+
+
 
 };
